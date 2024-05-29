3c6e24db6786396b6b9bfa234083ebacb85b825a
pakalamandasagar <pakalamandasagar955@gmail.com>
2024-05-29 16:36:14 +0530

    roboshop shell scripts

---
+++ redis.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+exec &>$LOGFILE 
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE 
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi 
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROE:: please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "you are root user"
+fi # fi means reverse of if, indicating codition end
+
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+VALIDATE $? "Installing Remi release"
+
+dnf module enable redis:remi-6.2 -y 
+
+VALIDATE $? "Enebaling redis"
+
+dnf install redis -y 
+
+VALIDATE $? "Installing redis"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf
+
+VALIDATE $? "Allowing remote connections"
+
+systemctl enable redis 
+
+VALIDATE $? "Enabling redis"
+
+systemctl start redis 
+
+VALIDATE $? "Starting redis"
+
+
+
