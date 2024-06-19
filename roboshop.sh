d4a3772cc19351f49df3d99b504fb23ec06654ed
pakalamandasagar <pakalamandasagar955@gmail.com>
2024-06-19 17:21:59 +0530

    daws-76s

--- roboshop.sh
+++ roboshop.sh
@@ -14,5 +14,5 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-053247f007b858182 --tag-specifications "ResourceType=instance,Tags=[{key=Name,Value=$i}]"
+    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-053247f007b858182 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done
