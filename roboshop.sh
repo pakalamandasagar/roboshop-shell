fb7431d62384f50683f5d72c07aad6faf473d849
pakalamandasagar <pakalamandasagar955@gmail.com>
2024-06-20 10:05:31 +0530

    daws-76s

--- roboshop.sh
+++ roboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AMI=ami-0b4f379183e5706b9
-SG_ID=sg-053247f007b858182 #replace with your SG ID
+SG_ID=sg-053247f007b858182 #Replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 ZONE_ID=Z053621117X5OWD6ZYBBM
 DOMAIN_NAME="daws77s.online"
@@ -18,7 +18,7 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-053247f007b858182 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
-    #create R53 records, make sure you delete existing record
+    #Create R53 records, make sure you delete existing record
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
